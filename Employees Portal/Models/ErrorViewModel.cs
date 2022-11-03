936424c1e2d190d9456845750d55298c2853a753
Anold Kupara <kuparaanold20@gmail.com>
2022-11-03 12:44:42 +0200

    Add project files.

---
+++ Employees Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employees_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
